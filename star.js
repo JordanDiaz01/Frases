fb2029aa7eed9383e74b3f8241c63359ba96a58e
JordanDiaz01 <jordandelangel.d@gmail.com>
2023-02-15 08:37:27 -0600

    star

---
+++ star.js
@@ -0,0 +1,26 @@
+const StarWarsCharacter = () =>{
+    return [
+                {
+                    "name": "Luke Skywalker",
+                    "height": "172",
+                    "mass": "77",
+                    "hair_color": "blond",
+                    "skin_color": "fair",
+                    "eye_color": "blue",
+                    "birth_year": "19BBY",
+                    "gender": "male"
+                },
+                {
+                    "name": "R2-D2",
+                    "height": "96",
+                    "mass": "32",
+                    "hair_color": "n/a",
+                    "skin_color": "white, blue",
+                    "eye_color": "red",
+                    "birth_year": "33BBY",
+                    "gender": "n/a"
+                }
+    ]
+}
+
+module.exports ={StarWarsCharacter}
